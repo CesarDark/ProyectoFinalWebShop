7ceda935367151ab4ec000705c889a7cda23d5f9
CesarDark <dark15crm@hotmail.com>
2019-05-27 23:33:50 -0500

    UPD: Nuevo formulario de contacto

--- contacto.php
+++ contacto.php
@@ -10,15 +10,13 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:400,300,500,600,700">
     <link rel="stylesheet" href="css/main.css">
 </head>
 
 <body>
     <!-- Navegación -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <img src="../Images/3_Spiderman_cara_editado_1200x1200.png" width="30" height="30" alt="">
         <a class="navbar-brand" href="index.php">The Geek Store</a>
         <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -38,79 +36,50 @@
             </ul>
         </div>
     </nav>
+    <!-- Navegación -->
 
-    <div class="row margin-top-bottom">
-           
-             <div class="col-md-offset-3 col-sm-offset-2 col-md-6 col-sm-8">   
-                 
-               <div class="row">
-                 <div class="contact-us-detail"><a href="mailto:info@uipasta.com">info@uipasta.com</a></div>
-                  <form class="contact-us pattern-bg">
-                    
-					  <div class="col-sm-6">
-						<div class="form-group">
-						  <input type="text" id="name" class="form-control" placeholder="Your Name">
-						 </div>
-                        </div>
-								
-                       <div class="col-sm-6">
-					    <div class="form-group">
-						 <input type="email" id="email" class="form-control" placeholder="Your Email">
-						 </div>
-                        </div>
-                        
-                        <div class="col-sm-6">
-						  <div class="form-group">
-							<input type="text" id="website" class="form-control" placeholder="Your Website">
-						   </div>
-                          </div>
-                          
-                        <div class="col-sm-6">
-						  <div class="form-group">
-							<input type="text" id="address" class="form-control" placeholder="Where are You From?">
-						   </div>
-                          </div>
-								
-                         <div class="col-sm-12">
-						  <select id="subject" class="form-group form-control">
-							<option value="" selected disabled>Subject</option>
-							<option>Website Design & Development</option>
-							<option>Wordpress Development</option>
-							<option>Search Engine Optimization</option>
-                            <option>Mobile Website</option>
-							<option>I Want to General Talk</option>
-                            <option>Other</option>
-						   </select>
-                          </div>
-								
-                       <div class="col-sm-12">
-					    <div class="textarea-message form-group">
-					      <textarea id="message" class="textarea-message form-control" placeholder="Your Message" rows="5"></textarea>
-						  </div>
-                         </div>
-                   
-                   
-                    <div class="text-center">      
-		               <button type="submit" class="button button-style button-style-dark button-style-color-2">Submit</button>
-	                  </div>
-                       
-                  </form>
-                  
-                  <p class="custom-margin">This Contact Form is a part of our free HTML template, Download the complete template from our website and It's fully 
-                  responsive and works perfect on all devices. Please Download from here - http://www.uipasta.com/webres-personal-resume-template/</p>
-                   
-                </div>
-			  </div>
-			  
-			  
-			  
-            </div>
-           
+    <div class="container">
+        <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="well well-sm">
+            <form class="form-horizontal" action="" method="post">
+            <fieldset>
+                <legend class="text-center">Contacto</legend>
         
+                <div class="form-group">
+                <label class="col-md-3 control-label" for="name">Nombre:</label>
+                <div class="col-md-9">
+                    <input id="name" name="name" type="text" placeholder="Nombre:" class="form-control">
+                </div>
+                </div>
+        
+                <div class="form-group">
+                <label class="col-md-3 control-label" for="email">Correo:</label>
+                <div class="col-md-9">
+                    <input id="email" name="email" type="text" placeholder="Correo:" class="form-control">
+                </div>
+                </div>
+        
+                <div class="form-group">
+                <label class="col-md-3 control-label" for="message">Mensaje:</label>
+                <div class="col-md-9">
+                    <textarea class="form-control" id="message" name="message" placeholder="Escribe tu mensaje..." rows="5"></textarea>
+                </div>
+                </div>
+                
+                <div class="form-group">
+                <div class="col-md-12 text-right">
+                    <button type="submit" class="btn btn-primary btn-lg">Enviar</button>
+                </div>
+                </div>
+            </fieldset>
+            </form>
+            </div>
         </div>
-
-
-
+        </div>
+    </div>
+    <div class="text-center">
+        <strong>&copy; Cesar Rebollar 2019</strong>
+    </div><br>
 </body>
-
 </html>
