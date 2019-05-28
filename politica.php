f648ae3b1972d2779e667f2801ea1b48decad1b2
CesarDark <dark15crm@hotmail.com>
2019-05-27 23:38:04 -0500

    UPD: Se agregó las politicas de la tienda

--- politica.php
+++ politica.php
@@ -9,6 +9,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <title>Política de Compra</title>
 </head>
 
@@ -16,6 +17,7 @@
 
     <!-- Navegación -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <img src="../Images/3_Spiderman_cara_editado_1200x1200.png" width="30" height="30" alt="">
         <a class="navbar-brand" href="index.php">The Geek Store</a>
         <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -35,5 +37,26 @@
             </ul>
         </div>
     </nav>
+    <!-- Navegación -->
+
+    <div class="text-center">
+        <h1 class="display-6">Política de Compra</h1>
+        <hr class="my-4">
+        <p class="lead">Compra</p>
+            <p>"Cómics solamente Digitales"</p>
+            <hr class="my-4">
+            <br>
+        <p class="lead">Pagos</p>
+            <p>• Tarjeta de Crédito.</p>
+            <p>• Paypal.</p>
+            <hr class="my-4">
+            <br>
+        <p class="lead">Envíos y Devoluciones</p>
+            <p> "No hay envíos, descrgas digitales.</p>
+            <p> "30 minutos a partir del pago."</p>
+            <hr class="my-4">
+            <br>
+        <strong>&copy; Cesar Rebollar 2019</strong>
+    </div>
 </body>
 </html>
