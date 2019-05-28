df930a9d6286c3c22177f21382c0edf5f0567773
CesarDark <dark15crm@hotmail.com>
2019-05-27 23:38:39 -0500

    UPD: Actualización de ¿Quienes somos? final

--- somos.php
+++ somos.php
@@ -9,6 +9,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <title>¿Quienes Somos?</title>
 </head>
 
@@ -16,6 +17,7 @@
 
     <!-- Navegación -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <img src="../Images/3_Spiderman_cara_editado_1200x1200.png" width="30" height="30" alt="">
         <a class="navbar-brand" href="index.php">The Geek Store</a>
         <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -35,17 +37,33 @@
             </ul>
         </div>
     </nav>
+    <!-- Navegación -->
 
-    <div class="jumbotron text-center">
-    <h1 class="display-4">¡Paso Final!</h1>
-    <hr class="my-4">
-    <p class="lead">
-        Estas a punto de pagar la cantidad de:
-        <h4> $<?php echo number_format($total, 2); ?></h4>
-        <div id="paypal-button-container"></div>
-    </p>
-    <p>Los productos podrán ser descargados una vez que se procese el pago</p>
-    <strong>(Para aclaraciones: cesar@gmail.com)</strong>
-</div>
+    <div class="text-center">
+        <h1 class="display-6">¿Quienes Somos?</h1>
+        <hr class="my-4">
+        <p class="lead">Misión</p>
+            <p>"Ofrecer los mejores Cómics a precios atractivos, proporcionando el mejor entretenimiento.</p>
+            <p>a personas de todas la edades en México".</p>
+            <hr class="my-4">
+            <br>
+        <p class="lead">Visión</p>
+            <p>"Ser una empresa líder en la venta de Cómics expandida por todo México y Sudamérica."</p>
+            <br>
+        <p class="lead">Valores</p>
+            <p>• Pasión: Disfrutar el mundo Geek.</p>
+            <p>• Excelencia: Buscar la perfección en nuestro producto.</p>
+            <p>• Calidad: Dar lo máximo a nuestros clientes.</p>
+            <hr class="my-4">
+            <br>
+        <p class="lead">Historia</p>
+            <p> "Empezamos por esta idea de vender Cómics por el simple hecho que nos apasiona este mundo.</p>
+            <p> Poco a poco este mercado se está expandiendo con el éxito de las películas de</p>
+            <p> superhéroes y teniendo el internet como medio de acceso a este contenido,</p>
+            <p> estamos seguros que muchas personas los comprarán."</p>
+            <hr class="my-4">
+            <br>
+        <strong>&copy; Cesar Rebollar 2019</strong>
+    </div>
 </body>
 </html>
