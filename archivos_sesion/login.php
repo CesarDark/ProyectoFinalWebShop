6d532085be724f001210ea99df46c5df027201b4
CesarDark <dark15crm@hotmail.com>
2019-05-27 23:27:59 -0500

    UPD: Login con sesión de usuario y admin y destroy session

--- archivos_sesion/login.php
+++ archivos_sesion/login.php
@@ -6,43 +6,28 @@
     $usuario = $_POST['usuario'];
     $password = md5($_POST['password']);
 
-    $query = "SELECT usuarioNombre, contraseña FROM usuarios WHERE usuarioNombre = '$usuario' AND contraseña = '$password'";
+    $query = "SELECT usuarioNombre, contraseña, rol 
+                FROM usuarios
+                WHERE usuarioNombre = '$usuario' 
+                AND contraseña = '$password'";
+
     $sql = $connect->prepare($query);
-    //$sql->bindParam(":usuarioNombre", $usuario);
-    //$sql->bindParam(":contraseña", $password);
     $sql->execute();
 
     $contador = $sql->rowCount();
-    if($contador ==1){ 
-        //pasamos los valores de la consulta a variable
-        $usuarioDB = $sql-> fetch(PDO::FETCH_ASSOC); //Guarda los registros que devuelve la SELECT en un array asociativo
-        print_r($usuarioDB);
-        $_SESSION['usuario'] = $usuario;
-        header('Location: ../principal.php');
-    }
+    $usuarioDB = $sql-> fetch(PDO::FETCH_ASSOC);
+
     
+    if ($usuarioDB){
+        if ($usuario == $usuarioDB['rol']){
+            $_SESSION['usuario'] = $usuario;
+            header('Location: ../admin/productos/gestorProduct.php');
 
-
-
-    // echo "Consulta ".$query;
-    // echo "<br>";
-    // var_dump($query);
-    // var_dump($result);
-
-    // $temp = pg_fetch_row($result);
-    // // var_dump($temp);
-    // // $pass = pg_fetch_row($result);
-    // $user = $temp[0];
-    // $pass = $temp[1];
-    // echo "Usuario: ".$user. " ".$pass;
-
-    // if (($usuario == $user) && ($password == $pass)) {
-    //     $_SESSION['usuario'] = $usuario;
-    //     // echo "La variable de sesión es: " .$_SESSION['username'];
-    //     header('Location: ../principal.php');
-    // } else {
-    //     // echo "No se puedo iniciar sesión";
-    //     session_destroy();
-    //     header('Location: ../index.php');
-    // }
+        } else {
+            $_SESSION['usuario'] = $usuario;
+            header('Location: ../principal.php');
+        }
+    } else {
+        header('Location: ../inicio.php');
+    }
 ?>
